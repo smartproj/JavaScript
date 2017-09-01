226dc0d8f223ee851f6400d24e485c6f74d67ff3
smartproj <31521536+smartproj@users.noreply.github.com>
2017-09-01 15:06:06 +0300

    Add files via upload

---
+++ project-numbers/numbers.js
@@ -0,0 +1,27 @@
+function myFunction4() {
+  var str = "4";
+  var result = str;
+  document.getElementById("td4").innerHTML = result;
+  document.getElementById("td4").style.fontSize = '36px';
+}
+
+function myFunction3() {
+  var str = "3";
+  var result = str;
+  document.getElementById("td3").innerHTML = result;
+  document.getElementById("td3").style.fontSize = '36px';
+}
+
+function myFunction2() {
+  var str = "2";
+  var result = str;
+  document.getElementById("td2").innerHTML = result;
+  document.getElementById("td2").style.fontSize = '36px';
+}
+
+function myFunction1() {
+  var str = "1";
+  var result = str;
+  document.getElementById("td1").innerHTML = result;
+  document.getElementById("td1").style.fontSize = '36px';
+}
